2991c7999bff5c033165c46096a4879b2e0447d2
Aaron Murray <aaron@aaron-murray.net>
2017-02-26 16:07:48 -0800

    Added Google Palette script, each line is colored now. Working on animation and chart details

--- app/scripts.babel/contentscript.js
+++ app/scripts.babel/contentscript.js
@@ -1,27 +1,44 @@
 'use strict';
 
+// GLOBALS
 var MAX_COLUMNS = 20;
-/*
-{
-  labels: [0,1,2,3,4,5,6],
-  data: {
-    'Aaron Phone': [],
-    'Shannon Phone': [],
-  }
-}
-*/
+var color_palette = palette('tol', 12);
+var host_counter = 0;
+var current_scale = 'KB';
+var scale_multipliers = {
+  'KB': 1,
+  'MB': 1000,
+  'GB': 1000000
+};
 var Global_Data = {};
 Global_Data.labels = [];
 Global_Data.data = {};
+
+function hexToRGB(hex) {
+  // Expand shorthand form (e.g. "03F") to full form (e.g. "0033FF")
+  var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
+  hex = hex.replace(shorthandRegex, function(m, r, g, b) {
+    return r + r + g + g + b + b;
+  });
+
+  var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+  return result ? {
+    r: parseInt(result[1], 16),
+    g: parseInt(result[2], 16),
+    b: parseInt(result[3], 16)
+  } : null;
+}
 
 function getTRData() {
   var returnObj = {};
   var dataObj = {};
   var rowData;
 
-  $('table.FormTable_NWM tr').not('.traffictable').not('.traffictable_footer').each( function(tr_index, tr_value) {
+  current_scale = $('#scale option:selected').text();
+
+  $('table.FormTable_NWM tr').not('.traffictable').not('.traffictable_footer').each(function(tr_index, tr_value) {
     rowData = [];
-    $(this).children().not('th').each(function(index, value){
+    $(this).children().not('th').each(function(index, value) {
       if ($(this).text() !== 'No data in table.') {
         rowData.push($(this).text()); // Raw data from the table.
         dataObj[rowData[0]] = rowData[1];
@@ -38,34 +55,46 @@ function getTRData() {
 
 function padData(what, thisManyTimes, where) {
   //console.log('PADDING DATA: ' + what + ', '+ thisManyTimes+ ' times, because: '+ where);
-  for (var i=0;i<thisManyTimes;i++) {
+  for (var i = 0; i < thisManyTimes; i++) {
     Global_Data.data[what].push(0);
   }
 }
 
 function updateDataObj(data) {
   // data = {
-	//   labels : [],
-	// 	datasets : []
+  //   labels : [],
+  // 	datasets : []
   // }
   var found;
-  var updatedItems = [];
+  var myPalette;
+  var paletteRGB;
+  var paletteRGBAplha;
 
-  data.labels = Global_Data.labels.slice(MAX_COLUMNS*-1);
+  data.labels = Global_Data.labels.slice(MAX_COLUMNS * -1);
 
   for (var item in Global_Data.data) {
     found = false;
-    for (var i=0;data.datasets[i];i++) {
+    for (var i = 0; data.datasets[i]; i++) {
       if (data.datasets[i].label === item) {
         found = true;
-        updatedItems.push(item);
-        data.datasets[i].data = Global_Data.data[item].slice(MAX_COLUMNS*-1);
+
+        data.datasets[i].data = Global_Data.data[item].slice(MAX_COLUMNS * -1);
       }
     }
 
     if (!found) {
-      updatedItems.push(item);
-      data.datasets.push({label: item, data: Global_Data.data[item].slice(MAX_COLUMNS*-1) })
+      myPalette = hexToRGB(color_palette[host_counter++]);
+      paletteRGB = 'rgba(' + myPalette.r + ',' + myPalette.g + ',' + myPalette.b + ',1)';
+      paletteRGBAplha = 'rgba(' + myPalette.r + ',' + myPalette.g + ',' + myPalette.b + ',0.5)';
+
+      data.datasets.push({
+        backgroundColor: paletteRGBAplha,
+        borderColor: paletteRGB,
+        pointBorderColor: paletteRGB,
+        pointStrokeColor: '#fff',
+        label: item,
+        data: Global_Data.data[item].slice(MAX_COLUMNS * -1),
+      });
     }
   }
 
@@ -83,7 +112,8 @@ function updateChartData(currentData, newData, newLabel) {
   if (!jQuery.isEmptyObject(newData)) {
 
     for (var ip in newData) {
-      newDataValue = newData[ip].split(' ')[0];
+      newDataValue = (parseFloat(newData[ip].split(' ')[0])) * scale_multipliers[current_scale];
+
       found = false;
       ip = ip.trim();
       for (var item in Global_Data.data) {
@@ -93,7 +123,7 @@ function updateChartData(currentData, newData, newLabel) {
           //console.log('UPDATING: '+ item);
           itemsUpdated.push(item);
           found = true;
-          padNumber = howManyLabels-Global_Data.data[item].length;
+          padNumber = howManyLabels - Global_Data.data[item].length;
           padData(item, padNumber, 'Update Existing');
 
           Global_Data.data[item].push(newDataValue);
@@ -115,7 +145,7 @@ function updateChartData(currentData, newData, newLabel) {
     //console.log(itemsUpdated);
     // Pad any existing hosts which were not otherwise updated this pass
     for (var host in Global_Data.data) {
-      if ( itemsUpdated.indexOf(host) === -1 ) {
+      if (itemsUpdated.indexOf(host) === -1) {
         //console.log('NOT MODIFIED: '+ host);
         padNumber = howManyLabels - Global_Data.data[host].length + 1;
         padData(host, padNumber, 'No Update');
@@ -136,36 +166,56 @@ $(function() {
   var outputTableContainer = $('#bwm-details-grid');
   var outputTable = $('table.FormTable_NWM');
   var outputTableRows = $('table.FormTable_NWM tr').not('.traffictable').not('.traffictable_footer');
+
+  current_scale = $('#scale option:selected').text();
   var theData = {};
   var newData = {};
 
-  var $canvas = $('<canvas>', {id: 'chart-container'});
+  var $canvas = $('<canvas>', {
+    id: 'chart-container',
+    class: 'chart-container'
+  });
   var data = {
-	  labels : [],
-		datasets : []
+    labels: [],
+    datasets: []
   };
   var ctx;
   var myChart;
   var count = 0;
   var chartOptions = {
     spanGaps: true,
-    animation: false,
-    backgroundColor: 'rgb(10,10,10)',
+    animation: {
+      duration: 500,
+      easing: 'linear'
+    },
+    title: {
+      display: true,
+      text: 'Data Transferred Per IP in KB'
+    },
+    scales: {
+      xAxes: [{
+        type: 'time',
+        unit: 'second',
+        unitStepSize: 6,
+        minUnit: 'second',
+      }]
+    }
   };
 
   // Insert chart canvas element
   outputTableContainer.before($canvas);
+  $('#chart-container').css('background-color', 'white');
 
   ctx = $('#chart-container').get(0).getContext('2d');
 
   myChart = new Chart(ctx, {
-      type: 'line',
-      data: data,
-      options: chartOptions
+    type: 'line',
+    data: data,
+    options: chartOptions
   });
 
   // Hide the original table cuz it stupid
-  outputTableContainer.hide();
+  //outputTableContainer.hide();
 
   // create an observer instance
   var observer = new MutationObserver(function(mutations) {
@@ -173,7 +223,7 @@ $(function() {
 
       newData = getTRData();
 
-      data = updateChartData(data, newData, count++);
+      data = updateChartData(data, newData, Date.now());
 
       myChart.update();
 
@@ -181,7 +231,12 @@ $(function() {
   });
 
   // configuration of the observer:
-  var config = { subtree: true, attributes: false, childList: true, characterData: true };
+  var config = {
+    subtree: true,
+    attributes: false,
+    childList: true,
+    characterData: true
+  };
 
   // pass in the target node, as well as the observer optionsf oooo
   observer.observe(outputTableContainer.get(0), config);
